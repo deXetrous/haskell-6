fbf7b29ebd131c26d6406b8bda75145011f89159
itsbhrat <35704185+itsbhrat@users.noreply.github.com>
2018-04-27 20:55:00 +0530

    modified rendering of characters

--- src/Game.hs
+++ src/Game.hs
@@ -2,8 +2,9 @@ module Game where
 
 import Data.Array
 
+
 data Player = PlayerB | PlayerW deriving (Eq, Show)
-data Cell = Empty | Full Player deriving (Eq, Show)
+data Cell = Empty | Full Player | Text1 | Text2 | Text3 | Text4 | Text5 | Text6 | Text7 | Text8 deriving (Eq, Show)
 data State = Running | GameOver (Maybe Player) deriving (Eq, Show)
 
 type Board = Array (Int, Int) Cell
@@ -14,13 +15,20 @@ data Game = Game { gameBoard :: Board
                  } deriving (Eq, Show)
 
 n :: Int           -- no of blocks(rows or columns)
-n = 8
+n = 9
 
 screenWidth :: Int
-screenWidth = 640
+screenWidth = 810
+
+screenWidth1 :: Int
+screenWidth1 =  720
 
 screenHeight :: Int
-screenHeight = 640
+screenHeight = 810
+
+screenHeight1 :: Int
+screenHeight1 = 720
+
 
 cellWidth :: Float
 cellWidth = fromIntegral screenWidth / fromIntegral n
@@ -31,7 +39,23 @@ cellHeight = fromIntegral screenHeight / fromIntegral n
 initialGame = Game { gameBoard = (array indexRange $ zip (range indexRange) (cycle [Empty])) // [ ((3, 3), Full PlayerB)
                                                                                                , ((3, 4), Full PlayerW)
                                                                                                , ((4, 4), Full PlayerB)
-                                                                                               , ((4, 3), Full PlayerW)]
+                                                                                               , ((4, 3), Full PlayerW)
+                                                                                               , ((8,1), Text1)
+                                                                                               , ((8,2), Text2)
+                                                                                               , ((8,3), Text3)
+                                                                                               , ((8,4), Text4)
+                                                                                               , ((8,5), Text5)
+                                                                                               , ((8,6), Text6)
+                                                                                               , ((8,7), Text7)
+                                                                                               , ((8,8), Text8)
+                                                                                               , ((0,0), Text8)
+                                                                                               , ((1,0), Text7)
+                                                                                               , ((2,0), Text6)
+                                                                                               , ((3,0), Text5)
+                                                                                               , ((4,0), Text4)
+                                                                                               , ((5,0), Text3)
+                                                                                               , ((6,0), Text2)
+                                                                                               , ((7,0), Text1)]
                    , gamePlayer =  PlayerB
                    , gameState = Running
                    }
--- src/Rendering.hs
+++ src/Rendering.hs
@@ -10,16 +10,22 @@ import Logic
 boardGridColor = makeColorI 128 128 128 255
 playerBColor = makeColorI 0 0 0 255
 playerWColor = makeColorI 255 255 255 255
+stringColor = makeColorI 0 0 0 255
 tieColor = greyN 0.5
 
-boardAsRunningPicture game board = 
+boardAsRunningPicture board =
 	pictures [ color playerBColor $ bCellsOfBoard board
 	         , color playerWColor $ wCellsOfBoard board
-	         , color boardColor $ boardGrid
+	         , color boardGridColor $ boardGrid
+					 , color stringColor $ s1CellsOfBoard board
+					 , color stringColor $ s2CellsOfBoard board
+					 , color stringColor $ s3CellsOfBoard board
+					 , color stringColor $ s4CellsOfBoard board
+					 , color stringColor $ s5CellsOfBoard board
+					 , color stringColor $ s6CellsOfBoard board
+					 , color stringColor $ s7CellsOfBoard board
+					 , color stringColor $ s8CellsOfBoard board
 	         ]
-	         where boardColor | gamePlayer game == PlayerB = playerBColor
-	                          | gamePlayer game == PlayerW = playerWColor
-	                          | otherwise = boardGridColor
 
 outcomeColor (Just PlayerB) = playerBColor
 outcomeColor (Just PlayerW) = playerWColor
@@ -38,14 +44,47 @@ wCell :: Picture
 wCell = circleSolid radius
 	where radius = cellWidth * 0.25
 
+
+sCell1 ::  Picture
+sCell1 = text str
+	where str = "1"
+
+sCell2 ::  Picture
+sCell2 = text str
+	where str = "2"
+
+sCell3 ::  Picture
+sCell3 = text str
+	where str = "3"
+
+sCell4 ::  Picture
+sCell4 = text str
+	where str = "4"
+
+sCell5 ::  Picture
+sCell5 = text str
+	where str = "5"
+
+sCell6 ::  Picture
+sCell6 = text str
+	where str = "6"
+
+sCell7 ::  Picture
+sCell7 = text str
+	where str = "7"
+
+sCell8 ::  Picture
+sCell8 = text str
+	where str = "8"
+
 cellsOfBoard :: Board -> Cell -> Picture -> Picture
-cellsOfBoard board cell cellPicture = 
+cellsOfBoard board cell cellPicture =
 	pictures
 	$ map (snapPictureToCell cellPicture . fst)
 	$ filter (\(_, e) -> e == cell)
 	$ assocs board
 
-	
+
 
 
 bCellsOfBoard :: Board -> Picture
@@ -54,8 +93,33 @@ bCellsOfBoard board = cellsOfBoard board (Full PlayerB) bCell
 wCellsOfBoard :: Board -> Picture
 wCellsOfBoard board = cellsOfBoard board (Full PlayerW) wCell
 
+s1CellsOfBoard :: Board -> Picture
+s1CellsOfBoard board = cellsOfBoard board (Text1) $ scale 0.25 0.25 sCell1
+
+s2CellsOfBoard :: Board -> Picture
+s2CellsOfBoard board = cellsOfBoard board (Text2) $ scale 0.25 0.25 sCell2
+
+s3CellsOfBoard :: Board -> Picture
+s3CellsOfBoard board = cellsOfBoard board (Text3) $ scale 0.25 0.25 sCell3
+
+s4CellsOfBoard :: Board -> Picture
+s4CellsOfBoard board = cellsOfBoard board (Text4) $ scale 0.25 0.25 sCell4
+
+s5CellsOfBoard :: Board -> Picture
+s5CellsOfBoard board = cellsOfBoard board (Text5) $ scale 0.25 0.25 sCell5
+
+s6CellsOfBoard :: Board -> Picture
+s6CellsOfBoard board = cellsOfBoard board (Text6) $ scale 0.25 0.25 sCell6
+
+s7CellsOfBoard :: Board -> Picture
+s7CellsOfBoard board = cellsOfBoard board (Text7) $ scale 0.25 0.25 sCell7
+
+s8CellsOfBoard :: Board -> Picture
+s8CellsOfBoard board = cellsOfBoard board (Text8) $ scale 0.25 0.25 sCell8
+
+
 boardGrid :: Picture
-boardGrid = 
+boardGrid =
 	pictures
 	$ concatMap (\i -> [ line [ (i * cellWidth, 0.0)
 		                      , (i * cellWidth, fromIntegral screenHeight)
@@ -66,10 +130,18 @@ boardGrid =
 		               	])
 	  [0.0 .. fromIntegral n]
 
-boardAsPicture board = 
+boardAsPicture board =
 	pictures [ bCellsOfBoard board
 	         , wCellsOfBoard board
 	         , boardGrid
+					 , s1CellsOfBoard board
+					 , s2CellsOfBoard board
+					 , s3CellsOfBoard board
+					 , s4CellsOfBoard board
+					 , s5CellsOfBoard board
+					 , s6CellsOfBoard board
+					 , s7CellsOfBoard board
+					 , s8CellsOfBoard board
 	         ]
 
 boardAsGameOverPicture winner board = color (outcomeColor winner) (boardAsPicture board)
@@ -79,5 +151,5 @@ gameAsPicture game = translate (fromIntegral screenWidth * (-0.5))
                                (fromIntegral screenHeight * (-0.5))
                                 frame
      where frame = case gameState game of
-     	            Running -> boardAsRunningPicture game (gameBoard game)
+     	            Running -> boardAsRunningPicture (gameBoard game)
      	            GameOver winner -> boardAsGameOverPicture winner (gameBoard game)
